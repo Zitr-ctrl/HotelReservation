09268114ebd6af33a7277bc22a88ac56543b2dd5
zitro <ortiz.ochoa.david.josue@gmail.com>
2026-02-13 14:07:08 -0500

    Sections Room added

---
+++ Controllers/HabitacionController.cs
@@ -0,0 +1,148 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaHotel.Data;
+using SistemaHotel.Models;
+
+namespace SistemaHotel.Controllers
+{
+    public class HabitacionesController : Controller
+    {
+        private readonly HotelContext _context;
+
+        public HabitacionesController(HotelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Habitaciones
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Habitaciones.ToListAsync());
+        }
+
+        // GET: Habitaciones/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var habitacion = await _context.Habitaciones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(habitacion);
+        }
+
+        // GET: Habitaciones/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Habitaciones/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Numero,Tipo,Capacidad,PrecioPorNoche,Disponible,Descripcion")] Habitacion habitacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(habitacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(habitacion);
+        }
+
+        // GET: Habitaciones/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var habitacion = await _context.Habitaciones.FindAsync(id);
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+            return View(habitacion);
+        }
+
+        // POST: Habitaciones/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Numero,Tipo,Capacidad,PrecioPorNoche,Disponible,Descripcion")] Habitacion habitacion)
+        {
+            if (id != habitacion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(habitacion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HabitacionExists(habitacion.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(habitacion);
+        }
+
+        // GET: Habitaciones/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var habitacion = await _context.Habitaciones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (habitacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(habitacion);
+        }
+
+        // POST: Habitaciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var habitacion = await _context.Habitaciones.FindAsync(id);
+            if (habitacion != null)
+            {
+                _context.Habitaciones.Remove(habitacion);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HabitacionExists(int id)
+        {
+            return _context.Habitaciones.Any(e => e.Id == id);
+        }
+    }
+}
